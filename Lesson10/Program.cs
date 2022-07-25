bff15fdb8f00c56a9e2f58a9a0170986647817a7
Юлия Алексина <ulchez@mail.ru>
2022-07-25 21:33:31 +0300

    Добавьте файлы проекта.

---
+++ Lesson10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { Corner corner;
+            do
+            {
+                Console.WriteLine("Задайте угол с помощью целочисленных значений");
+                int gradus = Convert.ToInt32(Console.ReadLine());
+                int min = Convert.ToInt32(Console.ReadLine());
+                int sec = Convert.ToInt32(Console.ReadLine());
+                corner = new Corner(gradus, min, sec);
+            }
+            while (corner.isCorrect == false);
+            
+            double rad = corner.ToRadians();
+            Console.WriteLine("Угол в радианах равен {0}", rad);
+            Console.ReadKey();
+        }
+    }
+}
